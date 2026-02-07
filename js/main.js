2b28707be2906dc2f50279cc9abf2cc42c32624d
JensBaumannDev <baumannjens@outlook.de>
2026-02-07 13:46:49 +0100

    legal notice and privacy policy added // styles adjusted - other content is hidden during link events

---
+++ js/main.js
@@ -0,0 +1,26 @@
+//#region LEGAL SWITCH
+function showLegal(section) {
+  document.getElementById("main-content").classList.add("d-none");
+
+  document.getElementById("impressum-content").classList.add("d-none");
+  document.getElementById("datenschutz-content").classList.add("d-none");
+
+  if (section === "impressum") {
+    document.getElementById("impressum-content").classList.remove("d-none");
+  } else if (section === "datenschutz") {
+    document.getElementById("datenschutz-content").classList.remove("d-none");
+  }
+
+  window.scrollTo(0, 0);
+}
+
+function showHome() {
+  document.getElementById("impressum-content").classList.add("d-none");
+  document.getElementById("datenschutz-content").classList.add("d-none");
+
+  document.getElementById("main-content").classList.remove("d-none");
+
+  window.scrollTo(0, 0);
+}
+
+//#endregion
