e34bbc2235e2daec7e2cc93d3b92b629cd26f997
JensBaumannDev <baumannjens@outlook.de>
2026-03-30 13:41:25 +0200

    Add page structure, folders, assets, and fonts

---
+++ src/app/components/colleagues-thoughts/colleagues-thoughts.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ColleaguesThoughts } from './colleagues-thoughts';
+
+describe('ColleaguesThoughts', () => {
+  let component: ColleaguesThoughts;
+  let fixture: ComponentFixture<ColleaguesThoughts>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ColleaguesThoughts],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ColleaguesThoughts);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/colleagues-thoughts/colleagues-thoughts.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-colleagues-thoughts',
+  imports: [],
+  templateUrl: './colleagues-thoughts.html',
+  styleUrl: './colleagues-thoughts.scss',
+})
+export class ColleaguesThoughts {}
