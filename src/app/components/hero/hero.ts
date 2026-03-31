f6284c6fc3e3f37556e6973a04535e4ed3041582
JensBaumannDev <baumannjens@outlook.de>
2026-03-31 17:45:47 +0200

    add: background-img / h1 headline with animations

--- src/app/components/hero/hero.ts
+++ src/app/components/hero/hero.ts
@@ -2,8 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
+  standalone: true,
   imports: [],
   templateUrl: './hero.html',
   styleUrl: './hero.scss',
 })
-export class Hero {}
+export class Hero {
+  headlineText = {
+    fullstack: ['F', 'u', 'l', 'l', 's', 't', 'a', 'c', 'k'],
+     developer: ['D', 'E', 'V', 'E', 'L', 'O', 'P', 'E', 'R'],
+  };
+
+  isUppercase(letter: string): boolean {
+    return letter === letter.toUpperCase();
+  }
+}
