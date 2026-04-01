e031981a4e313f3c4169f04203b473a5812fd85b
JensBaumannDev <baumannjens@outlook.de>
2026-04-01 17:42:37 +0200

    add name animation incl. translate

--- src/app/components/hero/hero.ts
+++ src/app/components/hero/hero.ts
@@ -1,16 +1,20 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { Navigation } from "../navigation/navigation";
-import {TranslatePipe, TranslateDirective} from '@ngx-translate/core';
+import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [Navigation,TranslatePipe,TranslateDirective],
+  imports: [Navigation,TranslatePipe],
   templateUrl: './hero.html',
   styleUrl: './hero.scss',
 })
 export class Hero {
   readonly isRightHovered = signal(false);
+  readonly isNameHovered = signal(false);
+  readonly nameLabelKey = computed(() =>
+    this.isNameHovered() ? 'hero.nameHover' : 'hero.nameDefault'
+  );
 
   headlineText = {
     fullstack: ['F', 'u', 'l', 'l', 's', 't', 'a', 'c', 'k'],
@@ -23,6 +27,10 @@ export class Hero {
 
   setRightHoverState(isHovered: boolean): void {
     this.isRightHovered.set(isHovered);
+  }
+
+  setNameHoverState(isHovered: boolean): void {
+    this.isNameHovered.set(isHovered);
   }
 }
 
