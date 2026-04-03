6e949c189ef4d92ef6f24cabe9f97331514befaa
JensBaumannDev <baumannjens@outlook.de>
2026-04-03 15:27:05 +0200

    add first parts of skill sections with placeholders

--- src/app/components/skill-set/skill-set.ts
+++ src/app/components/skill-set/skill-set.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
   templateUrl: './skill-set.html',
   styleUrl: './skill-set.scss',
 })
-export class SkillSet {}
+export class SkillSet {
+  skills = [
+    { name: 'HTML',           icon: 'icons/skills/html.svg' },
+    { name: 'CSS',            icon: 'icons/skills/css.svg' },
+    { name: 'JavaScript',     icon: 'icons/skills/javascript.svg' },
+    { name: 'TypeScript',     icon: 'icons/skills/typescript.svg' },
+    { name: 'Angular',        icon: 'icons/skills/angular.svg' },
+    { name: 'Supabase',       icon: 'icons/skills/supabase.svg' },
+    { name: 'Git',            icon: 'icons/skills/git.svg' },
+    { name: 'REST-API',       icon: 'icons/skills/rest-api.svg' },
+    { name: 'Scrum',          icon: 'icons/skills/scrum.svg' },
+    { name: 'Material Design',icon: 'icons/skills/material-design.png' },
+  ];
+}
