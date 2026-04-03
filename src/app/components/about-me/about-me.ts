fd930cbdc950f089bf7197db2a11e462838d1c2b
JensBaumannDev <baumannjens@outlook.de>
2026-04-03 13:11:31 +0200

    added oninit for responsive to change the pictures with smaller size for better performance

--- src/app/components/about-me/about-me.ts
+++ src/app/components/about-me/about-me.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal, OnInit, OnDestroy, NgZone } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
@@ -8,4 +8,22 @@ import { TranslatePipe } from '@ngx-translate/core';
   templateUrl: './about-me.html',
   styleUrl: './about-me.scss',
 })
-export class AboutMe {}
+export class AboutMe implements OnInit, OnDestroy {
+  isMobile = signal(false);
+
+  private mobileQuery!: MediaQueryList;
+  private listener = (e: MediaQueryListEvent) =>
+    this.ngZone.run(() => this.isMobile.set(e.matches));
+
+  constructor(private ngZone: NgZone) {}
+
+  ngOnInit() {
+    this.mobileQuery = window.matchMedia('(max-width: 768px)');
+    this.isMobile.set(this.mobileQuery.matches);
+    this.mobileQuery.addEventListener('change', this.listener);
+  }
+
+  ngOnDestroy() {
+    this.mobileQuery.removeEventListener('change', this.listener);
+  }
+}
