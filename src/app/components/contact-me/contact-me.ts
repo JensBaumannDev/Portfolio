dddadebcf08d102da4a3cf722faf3552e1095993
JensBaumannDev <baumannjens@outlook.de>
2026-04-07 20:07:18 +0200

    added full footer with responsive | added left content of contact_me

--- src/app/components/contact-me/contact-me.ts
+++ src/app/components/contact-me/contact-me.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
+import { Footer } from "../footer/footer";
 
 @Component({
   selector: 'app-contact-me',
   standalone: true,
-  imports: [],
+  imports: [TranslatePipe, Footer],
   templateUrl: './contact-me.html',
   styleUrl: './contact-me.scss',
 })
