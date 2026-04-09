61e789773c7754f3df628c507ce314bb50961184
JensBaumannDev <baumannjens@outlook.de>
2026-04-09 03:20:40 +0200

    add legal notice with translation (text is only an placeholder update coming soon)

--- src/app/pages/legal-notice/legal-notice.ts
+++ src/app/pages/legal-notice/legal-notice.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { Navigation } from "../../components/navigation/navigation";
+import { TranslatePipe } from '@ngx-translate/core';
+import { Footer } from "../../components/footer/footer";
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [Navigation],
+  imports: [Navigation, TranslatePipe, Footer],
   templateUrl: './legal-notice.html',
   styleUrl: './legal-notice.scss',
 })
