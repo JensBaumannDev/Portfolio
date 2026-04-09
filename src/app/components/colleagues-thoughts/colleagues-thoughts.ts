45ec45e8887856d75a20221496491efd89a91c0e
JensBaumannDev <baumannjens@outlook.de>
2026-04-09 20:20:59 +0200

    added "in their word" section with all animations an responsive to 320px // text is placeholder

--- src/app/components/colleagues-thoughts/colleagues-thoughts.ts
+++ src/app/components/colleagues-thoughts/colleagues-thoughts.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-colleagues-thoughts',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './colleagues-thoughts.html',
   styleUrl: './colleagues-thoughts.scss',
 })
