a66ca9888f89f42eb4d7c3689c00b02c6b806372
JensBaumannDev <baumannjens@outlook.de>
2026-04-12 14:16:38 +0200

    add  full procets with animations, styles and translations without responsive

--- src/app/components/projects/projects.ts
+++ src/app/components/projects/projects.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
+
+interface Project {
+  title: string;
+  technologies: string[];
+  descriptionKey: string;
+  image: string;
+  githubLink: string;
+  liveLink: string;
+}
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './projects.html',
   styleUrl: './projects.scss',
 })
-export class Projects {}
+export class Projects {
+  projects: Project[] = [
+    {
+      title: 'Join',
+      technologies: [],
+      descriptionKey: 'projects.join.description',
+      image: 'img/06_projects/Laptop.png',
+      githubLink: 'https://github.com/JensBaumannDev/Join',
+      liveLink: 'https://jens-baumann.developerakademie.net/join/index.html',
+    },
+    {
+      title: 'El Pollo Loco',
+      technologies: ['JavaScript', 'HTML', 'CSS'],
+      descriptionKey: 'projects.pollo.description',
+      image: 'img/06_projects/Pollo.png',
+      githubLink: 'https://github.com/JensBaumannDev/el-pollo-loco',
+      liveLink: 'https://jens-baumann.developerakademie.net/el-pollo-loco/index.html',
+    },
+    {
+      title: 'Pokedex',
+      technologies: ['JavaScript', 'HTML', 'CSS', 'Api'],
+      descriptionKey: 'projects.pokedex.description',
+      image: 'img/06_projects/pokedex.png',
+      githubLink: 'https://github.com/JensBaumannDev/pokedex',
+      liveLink: 'https://jens-baumann.developerakademie.net/pokedex/index.html',
+    },
+  ];
+}
