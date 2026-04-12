1516f0483210725f1f61f8c616496e05b9ac3952
JensBaumannDev <baumannjens@outlook.de>
2026-04-12 21:58:29 +0200

    bugfix bg on legal notice with performance boost

--- src/app/pages/legal-notice/legal-notice.ts
+++ src/app/pages/legal-notice/legal-notice.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Navigation } from "../../components/navigation/navigation";
 import { TranslatePipe } from '@ngx-translate/core';
 import { Footer } from "../../components/footer/footer";
+import { NgOptimizedImage } from "@angular/common";
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [Navigation, TranslatePipe, Footer],
+  imports: [Navigation, TranslatePipe, Footer, NgOptimizedImage],
   templateUrl: './legal-notice.html',
   styleUrl: './legal-notice.scss',
 })
